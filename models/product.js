be13e52f15a4ba018e95a8a3298047395553becf
Elsharabasy <abdo.make73@.com>
2023-11-18 22:17:01 +0200

    replace product module code

--- models/product.js
+++ models/product.js
@@ -1,25 +1,27 @@
-const db = require("../util/database");
+const Sequelize = require("sequelize");
 
-const Cart = require("./cart");
+const sequelize = require("../util/database");
 
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {}
-
-  static deleteById(id) {}
-
-  static fetchAll() {
-    return db.execute("select * from new_table");
-  }
-
-  static findById(id) {}
-};
+module.exports = Product;
