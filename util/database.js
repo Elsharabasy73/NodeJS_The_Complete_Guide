0da8a19b7e1b2696ac23391bfb5715914907cdac
Elsharabasy <abdo.make73@.com>
2023-11-21 20:29:11 +0200

    [add](180)Installing and setting

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
 
-const sequelize = new Sequelize("node-complete", "root", "nodecomplete", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (cb) => {
+  const MongoClient = mongodb.MongoClient;
+  //create a connection
+  MongoClient.connect(
+    "mongodb+srv://abdomake73:xlsgzIvu2CYeOTrg@cluster0.vclsggt.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((result) => {
+      console.log("conneced");
+      cb(result);
+    })
+    .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
