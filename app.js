1c32827bc00cb6cecbdf1cb50c20ee61114fca73
Elsharabasy <abdo.make73@.com>
2023-11-22 19:47:49 +0200

    [add](194) Creating New Users

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
-
+const User = require("./models/user");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -13,9 +13,19 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const { log } = require("console");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("655e3d92246841c43876c6d7")
+    .then((user) => {
+      req.user = user;
+    })
+    .catch((err) => console.log(err));
+    next();
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
