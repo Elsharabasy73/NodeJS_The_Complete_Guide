a487dfe914c8916e96078ae1199cd8e29a6a2922
Elsharabasy <abdo.make73@.com>
2023-11-23 21:16:48 +0200

    [add](198) add prods to db's cart

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ class User {
     this.name = name;
     this.email = email;
     this.cart = cart; //{}
-    this._id = id ? new mongodb.ObjectId(id) : null;
+    this._id = id ? new ObjectId(id) : null;
   }
 
   save() {
@@ -16,21 +16,47 @@ class User {
   }
 
   addToCart(product) {
-    const updatedCart = { items: [{productId: new mongodb.ObjectId(product._id), quantity: 1 }] };
-    const db = getDb();
+    //s/earch for item.
+    console.log('cartt', this.cart)
+    console.log('itemst', this.cart.items)
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      console.log("Product ID:", product._id);
+      console.log("Cart Product ID:", cp.productId);
+      return product._id.toString() === cp.productId.toString();
+    });
+    console.log("cartProductIndex", cartProductIndex);
+    let newQty = 1;
+    const updatedCartItems = [...this.cart.items];
+    //item already exist
+    if (cartProductIndex >= 0) {
+      console.log('update product')
+      newQty = this.cart.items[cartProductIndex].quantity + 1;
+      //forgot .quantity
+      updatedCartItems[cartProductIndex].quantity = newQty;
+      //first time for this item.
+    } else {
+      console.log('product first')
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQty,
+      });
+    }
 
-    db
-      .collection("users").findOne({_id: this._id}).then(user=>{
-        console.log('done', user);
-      })
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+    // const updatedCart = { items: [{productId: new mongodb.ObjectId(product._id), quantity: 1 }] };
+    
+    const db = getDb();
     return db
       .collection("users")
-      .updateOne({ _id: this._id}, { $set: { cart: updatedCart } });
-  }
+      .updateOne({ _id: this._id }, { $set: { cart: updatedCart } });
+  }    
+
 
   static findById(userId) {
     const db = getDb();
-    const mongoIdForm = new mongodb.ObjectId(userId);
+    const mongoIdForm = new ObjectId(userId);
     console.log(mongoIdForm);
     return db
       .collection("users")
