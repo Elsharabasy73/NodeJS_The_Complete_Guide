1fc9a40ad43e7a2d5175719ce25c0df312c669ed
Elsharabasy <abdo.make73@.com>
2023-11-27 15:04:30 +0200

    [add](218) Adding and Using a User Model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,15 +17,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("655e3d92246841c43876c6d7")
-//     .then((user) => {
-//       //user came from the db
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("65639f553e44f96de15b9436")
+    .then((user) => {
+      //user came from the db
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -36,5 +36,17 @@ mongoose
   .connect(
     "mongodb+srv://abdomake73:xlsgzIvu2CYeOTrg@cluster0.vclsggt.mongodb.net/shop?retryWrites=true&w=majority"
   )
-  .then((result) => app.listen(3000))
+  .then((result) => {
+    User.findOne().then(user=>{
+      if(!user){
+        const user = new User({
+          name: "abdo",
+          email: "abdo@test.com",
+          cart: { items: [] },
+        })
+        user.save();
+      }
+    })
+    app.listen(3000);
+  })
   .catch((err) => console.log(err));
