83cbeb7ea20249c092ee9cbc6d008b3eb079c363
Elsharabasy <abdo.make73@.com>
2023-12-10 23:22:53 +0200

    [add]294Built-In & Custom Validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,35 @@
-const express = require('express');
+const express = require("express");
 
-const authController = require('../controllers/auth');
-const { check } = require('express-validator')
+const authController = require("../controllers/auth");
+const { check } = require("express-validator");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.'), authController.postSignup);
+router.post(
+  "/signup",
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .custom((value, { req }) => {
+      if (value === "abdo") {
+        throw new Error("this email is forbidden");
+      }
+      return true;
+    }),
+  authController.postSignup
+);
 
-router.post('/logout', authController.postLogout);
+router.post("/logout", authController.postLogout);
 
-router.get('/reset', authController.getReset);
+router.get("/reset", authController.getReset);
 
-router.post('/reset', authController.postReset);
+router.post("/reset", authController.postReset);
 
 router.get("/reset/:token", authController.getNewPassword);
 
