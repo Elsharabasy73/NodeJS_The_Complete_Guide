ea46c85f3a34569fac7bf3213b6732bfa678d925
abdomake73 <abdo.make73@gmail.com>
2023-12-19 20:53:25 +0200

    [fix] render insred of redirecting

--- app.js
+++ app.js
@@ -84,7 +84,11 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redirect("/500");
+  res.status(500).render("500", {
+    pageTitle: "Page Not Found",
+    path: "/500",
+    isAuthenticated: req.isLoggedIn,
+  });
 });
 
 mongoose
